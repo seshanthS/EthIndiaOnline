f70c51516ea096e7cc643e07e33340a73b0c46dc
seshanthS <seshanth.s@hotmail.com>
2020-02-28 21:50:34 +0530

    pushing to server

--- api/routes/index.js
+++ api/routes/index.js
@@ -29,30 +29,112 @@ router.post('/getPayments', async(req,res)=>{
 
 })
 
+router.post('/withdraw', async(req,res)=>{
+    try{
+        await paymentsModel.updateOne({email: req.body.receiverEmail}/* {$and:[{email: req.body.receiverEmail}, {'payments.senderAddress': req.body.senderAddress}]} */,
+            {
+                $pull:{
+                    'payments':{
+                        senderAddress:  req.body.senderAddress
+                    }
+                }
+            }).exec()
+
+            res.status(200).send({
+                error: 'nil',
+                status: 'success',
+                data: ''
+            })
+
+    }catch(err){
+        console.log(err.message)
+        res.status(500).send({
+            error: err.message,
+            status: 'fail',
+            data: ""
+        })
+    }
+})
+
+router.post('/getPendingAmount', async(req,res)=>{
+    let pending
+    try{
+        let query = await paymentsModel.findOne({$and:[{email: req.body.receiverEmail},
+            {'payments.senderAddress': req.body.senderAddress}]}).lean().exec()
+
+        if(query != null){
+            console.log(query)
+            let pendingAmounts = query.payments
+            pending = pendingAmounts.filter((_amountObject)=>{
+                    return _amountObject.senderAddress == req.body.senderAddress
+            })
+        }
+        
+
+        res.status(200).send({
+            error: 'nil',
+            status: 'success',
+            pendingAmount: Number(pending[0].amount)
+        })
+    }catch(err){
+        console.log(err.message)
+        res.status(500).send({
+            error: 'err.message',
+            status: 'fail',
+            pendingAmount: ''
+        })
+    }
+})
+
 router.post('/add', async(req,res)=>{
     let {receiverEmail, signature, message, amount, senderAddress} = req.body
 
     try{
-        await paymentsModel.updateOne({
-            email: receiverEmail
-        }, {
-            email: receiverEmail,
-            $push:{
-                payments: [{
-                    signature: signature,
-                    message: message,
-                    amount: amount,
-                    senderAddress, senderAddress
-                }]
-            }
-            
-        }, {upsert: true}).exec()
 
+        let data = await paymentsModel.find({$and:[{email: receiverEmail},
+            {'payments.senderAddress': senderAddress}]}).lean().exec()
+
+        if(data.length > 0){
+            //user has already opened a channel with receiverEmail.
+            await paymentsModel.updateOne({
+                $and:[
+                    {email: receiverEmail},
+                    {'payments.senderAddress': senderAddress}
+                ]
+            },
+            {
+                email: receiverEmail,
+                $set:{
+                    'payments.$.signature': signature,
+                    'payments.$.message' : message,
+                    'payments.$.amount': amount,
+                    'payments.$.senderAddress': senderAddress
+                }
+            }).exec()
+        }else{
+            await paymentsModel.updateOne({
+                email: receiverEmail
+            }, {
+                email: receiverEmail,
+                $push:{
+                    payments: [{
+                        signature: signature,
+                        message: message,
+                        amount: amount,
+                        senderAddress, senderAddress
+                    }]
+                }
+                
+            }, {upsert: true}).exec()
+        }
+
+        
         res.status(200).send({   
             error: 'nil',
             status: 'success',
             data: ''
         })
+
     }catch(err){
         res.status(500).send({   
             error: err.message,
