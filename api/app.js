0c438de42f0721304b7a3341ecef6ec272dd5a86
seshanthS <seshanth.s@hotmail.com>
2020-02-29 20:39:50 +0530

    added additional headers to cors

--- api/app.js
+++ api/app.js
@@ -21,12 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
- });
- 
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
